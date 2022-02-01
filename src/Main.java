5a6bc095fe9f74a805a82262d3fee54e45a7d3f6
Lukas Schauhuber <35468672+luggaesch@users.noreply.github.com>
2022-02-01 16:51:28 +0100

    added commentary

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
 
+    /*
+        Verwenden Sie diese Klasse bzw. Methode als Startpunkt für ihre Anwendung.
+        Erstellen Sie zuvor eine 'Customer'-Klasse, die, gemäß der Aufgabenstellung, mit der 'Restaurant'-Klasse arbeitet.
+     */
     public static void main(String[] args) {
-        /*
-            Hier selbst implementieren, vorher Customer Klasse erstellen
-         */
+        System.out.println("Hello World!");
     }
 }
--- src/Restaurant.java
+++ src/Restaurant.java
@@ -2,6 +2,10 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/*
+    Bei einem Restaurant kann Essen bestellt werden. Da die Zubereitung 'asynchron' verläuft, erwartet das Restaurant
+    einen sogenannten Observer, den es über das Ereignis 'Essen lieferbereit/geliefert' informieren kann.
+ */
 public class Restaurant {
 
     private final Cuisine cuisine;
@@ -13,11 +17,19 @@ public class Restaurant {
         this.maxDeliveryTime = maxDeliveryTime;
     }
 
+    /*
+        Verwenden Sie diese Methode um ein zufälliges Restaurant zu erhalten.
+        Es wird ein zufällige Nationalität, sowie eine maximale Lieferzeit errechnet.
+     */
     public static Restaurant getRandomRestaurant() {
         Cuisine randomCuisine = Cuisine.values()[random.nextInt(Cuisine.values().length)];
         return new Restaurant(randomCuisine, random.nextInt(5000, 15000));
     }
 
+    /*
+        Die order-Methode wird verwendet, um eine Bestellung in Auftrag zu geben.
+        Sobald das Essen zubereitet ist, wird der horchende Observer/Listener über das Ereignis informiert.
+     */
     public void order(String dish, TakeoutListener listener) {
         Timer timer = new Timer();
         Restaurant restaurant = this;
